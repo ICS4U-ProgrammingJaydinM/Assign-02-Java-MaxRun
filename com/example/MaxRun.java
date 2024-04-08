3cf8fa1dcce276617def08a900dba7349b736b1f
Jaydin Madore <92885447+JaydinM@users.noreply.github.com>
2024-04-08 17:57:27 -0400

    Initial commit

---
+++ com/example/MaxRun.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Jaydin Madore
+ * @version 1.0
+ * @since 2024-04-08
+ */
+
+// MaxRun class
+public final class MaxRun {
+
+    /** Private constructor to prevent instantiation. */
+    private MaxRun() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
